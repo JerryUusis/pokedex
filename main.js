c38e3e3ec451fb5d8e6ea5f6ce1fc8b73c2f6e8a
JerryUusis <jerry.uusitalo@gmail.com>
2023-10-04 15:13:20 +0300

    Add sketch of search functionality

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
 const cardContainer = document.querySelector(".card-container");
 const typeContainer = document.querySelector(".type-container");
+const searchBar = document.querySelector("#search");
 let pokeData = [];
+
+searchBar.addEventListener("keyup", (event) => {
+    console.log(event.target.value)
+    pokeData.filter(pokemon => {
+        pokemon.name.contain(event)
+    })
+})
 
 async function fetchData(url) {
     await fetch(url)
@@ -26,9 +34,12 @@ async function fetchData(url) {
         })
 }
 
+// 2. connect input and search from pokeDEx array by using
+// the .filter() method
+// https://www.jamesqquick.com/blog/build-a-javascript-search-bar/
+
 function pokeCards() {
     const cards = pokeData.map(pokemon => {
-        console.log(pokemon)
         return `<div class="data-card">
         <img src="${pokemon.img}" alt="Picture of ${pokemon.name}" class="card-image">
         <div class="text-container">
