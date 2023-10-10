fc0420d488d3258ffbbf5ff82463c926c4d53b95
JerryUusis <jerry.uusitalo@gmail.com>
2023-10-10 12:20:45 +0300

    Add fetches for properties and display cards dynamically

--- main.js
+++ main.js
@@ -14,17 +14,23 @@ async function fetchData() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
     const data = await response.json();
 
-    fetchPokemonData(data.results);
+    pokeData = await fetchPokemonProperties(data.results);
+
     pokeCards();
 }
 
-// Gets data from main array and turns it into json and pushes it into to pokeData array
-async function fetchPokemonData(array) {
-    array.map(async (pokemon) => {
-        const response = await fetch(pokemon.url);
+async function fetchPokemonProperties(array) {
+    const pokemonPromises = array.map(async (item) => {
+        const response = await fetch(item.url);
         const data = await response.json();
-        pokeData.push(data);
+        return {
+            id: data.id,
+            name: data.name,
+            img: data.sprites.other['official-artwork'].front_default,
+            types: data.types,
+        };
     })
+    return Promise.all(pokemonPromises)
 }
 
 // 2. connect input and search from pokeDEx array by using
@@ -33,20 +39,21 @@ async function fetchPokemonData(array) {
 
 function pokeCards() {
     const cards = pokeData.map(pokemon => {
-        return `            <div class="data-card">
-        <img src="img/paras.png" alt="Image of pokemon" class="card-image">
-        <div class="text-container">
-            <h2 class="name">Paras</h2>
-            <div class="properties-container">
-                <p>Type 1</p>
-                <p>Type 2</p>
-                <p>Kanto</p>
-                <p>Height: 50cm</p>
-                <p>Weight: 10kg</p>
-            </div>
-        </div>
-    </div>`
+        return `<div class="data-card">
+                    <img src="${pokemon.img}" alt="Image of pokemon" class="card-image">
+                    <div class="text-container">
+                        <h2 class="name">${pokemon.name}</h2>
+                        <div class="properties-container">
+                            <p>Type 1</p>
+                            <p>Type 2</p>
+                            <p>Kanto</p>
+                            <p>Height: 50cm</p>
+                            <p>Weight: 10kg</p>
+                        </div>
+                    </div>
+                </div>`
     }).join("");
     cardContainer.innerHTML = cards;
 }
 
+fetchData();
